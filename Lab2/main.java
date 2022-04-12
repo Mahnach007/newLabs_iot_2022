08313e0d44322472a0ed1ed540234136e7bea1b7
Vlad Gotovchykov <vladyslav.hotovchykov.ir.2021@lpnu.ua>
2022-04-12 16:34:03 +0200

    First Commit of Lab2

---
+++ Lab2/main.java
@@ -0,0 +1,23 @@
+package Lab2;
+
+
+
+public class main {
+	
+	
+
+	public static void main (String[] agrs)  {
+	     
+	     Computer computer = new Computer(16, "dick", "Sumsung", "slut", "Rtx 3090");
+	     Phone phone = new Phone(13, "Iphone", "Xiaomi", 13, "Rtx 3090");
+	     Monitor monitor = new Monitor(15, "DX Racer", "Apple", 27.7, "");
+	     
+	     System.out.println(computer);
+	     System.out.println(phone);
+	     System.out.println(monitor);
+	    
+
+	  }
+
+	
+}
