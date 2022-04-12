019727608188bcd5e73a25611ac809426fbc42f5
Vlad Gotovchykov <vladyslav.hotovchykov.ir.2021@lpnu.ua>
2022-04-12 17:28:35 +0200

    changed names 2

--- Lab2/main.java
+++ Lab2/main.java
@@ -8,9 +8,9 @@ public class main {
 
 	public static void main (String[] agrs)  {
 	     
-	     Computer computer = new Computer(16, "dick", "Sumsung", "slut", "Rtx 3090");
+	     Computer computer = new Computer(16, "Telifonia", "Sumsung", "MOS-1234", "Rtx 3090");
 	     Phone phone = new Phone(13, "Iphone", "Xiaomi", 13, "Rtx 3090");
-	     Monitor monitor = new Monitor(15, "DX Racer", "Apple", 27.7, "");
+	     Monitor monitor = new Monitor(15, "DX Racer", "Apple", 27.7, "720x1080");
 	     
 	     System.out.println(computer);
 	     System.out.println(phone);
